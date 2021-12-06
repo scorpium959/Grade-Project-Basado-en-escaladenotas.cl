bdbfe0d150e419dc3a16110dba273b4dfc883965
REvenge_gamES <64052400+scorpium959@users.noreply.github.com>
2021-12-05 22:40:45 -0300

    Update gradeproject.lua

--- gradeproject.lua
+++ gradeproject.lua
@@ -6,6 +6,11 @@ function round(numero, decimales)
     local power = 10^decimales
     return math.floor(numero * power) / power
 end
+function pregunta(dato) --El dato siempre debe ser string
+  io.write('\n'.."Ingrese " .. dato, ": ")
+  local input = error_checker_num(io.read())
+  return input
+end
 function is_file_exists(nombre)
     local file = io.open(nombre, "r")
     if file == nil then
@@ -16,45 +21,24 @@ function is_file_exists(nombre)
 end
 function error_checker_num(Variable)
   while tonumber(Variable) == nil do
-    --io.write('\n', type(Variable))
-    io.write('\n', "Pruebe de nuevo, use numeros en vez de letras o caracteres: ")
-    Variable = io.read()
+      io.write('\n', "Pruebe de nuevo, use numeros en vez de letras o caracteres: ")
+      Variable = io.read()
   end
   return tonumber(Variable)
 end
 
-repetir = "si"
-while (repetir == "si" or repetir == "Si" or repetir == "SI" or repetir == "sI") do
+repeat
     io.write('\n', "Notese poner punto(.) en vez de coma (,) en notas")
-    io.write('\n', "ingrese nota maxima: ")
-    notamax = io.read()
-    notamax = error_checker_num(notamax)
-
-    io.write('\n',"ingrese nota minima: ")
-    notamin = io.read()
-    notamin = error_checker_num(notamin)
-
-    io.write('\n', "ingrese nota minima de aprovacion: ")
-    notaapro = io.read()
-    notaapro = error_checker_num(notaapro)
-
-    io.write('\n', "Escriba porcentaje de exigencia en decimal(por ejemplo 60% = 0.6): ")
-    exigencia = io.read()
-    exigencia = error_checker_num(exigencia)
-
-    io.write('\n', "Escriba el puntaje maximo: ")
-    punmax = io.read()
-    punmax = error_checker_num(punmax)
-
-    io.write('\n', "Ingrese el puntaje obtenido: ")
-    puntaje = io.read()
-    puntaje = error_checker_num(puntaje)
+    notamax = pregunta("nota maxima")
+    notamin = pregunta("nota minima")
+    notaapro = pregunta("nota minima de aprobacion")
+    exigencia = pregunta("porcentaje de exigencia (por ejemplo: 60 sin simbolo de porcentaje)") / 100
+    punmax = pregunta("puntaje maximo")
+    puntaje = pregunta("puntaje obtenido")
 
     while (puntaje > punmax) do
-      io.write('\n', "Puntaje Mayor que el puntaje maximo, ingrese el puntaje de nuevo:")
-      puntaje = io.read()
-      error_checker_num(puntaje)
-      puntaje = tonumber(puntaje)
+      io.write('\n', "Puntaje Mayor que el puntaje maximo.")
+      puntaje = pregunta("el puntaje de nuevo")
     end
     if (puntaje < exigencia * punmax) then
         n = (notaapro - notamin) * (puntaje / (exigencia * punmax)) + notamin
@@ -63,14 +47,13 @@ while (repetir == "si" or repetir == "Si" or repetir == "SI" or repetir == "sI")
         n = (notamax - notaapro) * ((puntaje - exigencia * punmax) / (punmax * (1 - exigencia))) + notaapro
         io.write('\n',"Su nota es: ", round(n, 1), '\n')
     end
-
     if is_file_exists("notas.txt") == true then
         file = io.open("notas.txt", "a")
-        file:write('\n','Puntaje maximo: ', punmax,', Puntaje: ',puntaje,', nota: ' , round(n, 1))
+        file:write('\n','Puntaje maximo: ', punmax,', Puntaje: ',puntaje,', Nota: ' , round(n, 1))
         file:close()
     else
         file = io.open("notas.txt", "w")
-        file:write('Puntaje maximo: ', punmax,', Puntaje: ',puntaje,', nota: ', round(n, 1))
+        file:write('Puntaje maximo: ', punmax,', Puntaje: ',puntaje,', Nota: ', round(n, 1))
         file:close()
     end
     io.write('\n', "Quiere repetir el proceso, (si/no): ")
@@ -79,4 +62,4 @@ while (repetir == "si" or repetir == "Si" or repetir == "SI" or repetir == "sI")
 		    io.write('\n', "Quiere repetir el proceso, (si/no): ")
 		    repetir = io.read()
 	  end
-end
+until (repetir == "no" or repetir == "n" or repetir == "No" or repetir == "NO" or repetir == "nO")
